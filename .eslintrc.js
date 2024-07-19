7af111bd431b6ad5850212f5a51d51b333df5bbe
carlosazeta <carlosagundezdev@gmail.com>
2024-07-19 19:35:11 +0200

    scrap metacritic

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+	extends: ['universe/native', 'prettier'],
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+		'react/react-in-jsx-scope': 'off',
+	},
+}
