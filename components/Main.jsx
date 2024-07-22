a7f8224a163f136ce92e864c72a4d97b1f1f116d
carlosazeta <carlosagundezdev@gmail.com>
2024-07-22 23:36:10 +0200

    Create GameCard component

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from 'react'
 import {
-	StyleSheet,
-	Text,
 	View,
-	Image,
 	ScrollView,
 } from 'react-native'
 import { getLatestGames } from '../lib/metacritic'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import GameCard from './GameCard'
 
 export function Main() {
 	const [games, setGames] = useState([])
@@ -23,12 +21,7 @@ export function Main() {
       <View style={{paddingTop: insets.top, paddingBottom: insets.bottom}}>
         <ScrollView>
           {games.map((game) => (
-            <View key={game.slug} style={styles.card}>
-              <Image source={{ uri: game.image }} style={styles.image} />
-              <Text style={styles.title}>{game.title}</Text>
-              <Text style={styles.description}>{game.description}</Text>
-              <Text style={styles.score}>{game.score}</Text>
-            </View>
+            <GameCard key={game.slug} game={game}/>
           ))}
         </ScrollView>
       </View>
@@ -36,30 +29,4 @@ export function Main() {
 	)
 }
 
-const styles = StyleSheet.create({
-	card: {
-		marginBottom: 42,
-	},
-	image: {
-		width: 107,
-		height: 147,
-		borderRadius: 10,
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: 'bold',
-		color: '#fff',
-		marginTop: 10,
-	},
-	description: {
-		fontSize: 16,
-		color: '#eee',
-	},
-	score: {
-		fontSize: 20,
-		fontWeight: 'bold',
-		color: 'green',
-		marginBottom: 10,
-	},
-	title: {},
-})
+
